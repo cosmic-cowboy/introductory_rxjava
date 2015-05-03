b5202862f8bc2ddd195e75201aa93d96186a3166
hiroo takaku <slgerkamp@gmail.com>
2015-05-03 17:18:51 +0900

    RxJavaの基本的な動きを確認する 別スレッドでスリープ

--- Introductory-rxjava/src/test/java/com/slgerkamp/introductory/rxjava/chapter01/HowToUseRxJavaTest.java
+++ Introductory-rxjava/src/test/java/com/slgerkamp/introductory/rxjava/chapter01/HowToUseRxJavaTest.java
@@ -1,11 +1,19 @@
 package com.slgerkamp.introductory.rxjava.chapter01;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 
 import rx.Observable;
+import rx.schedulers.Schedulers;
 
 /**
  * <p>参考：7つのサンプルプログラムで学ぶRxJavaの挙動
@@ -33,5 +41,34 @@ public class HowToUseRxJavaTest {
 		assertThat(sb.toString(), is("start:maponNextonCompleteend"));
 	}
 
+	@Test
+	public void RxJavaの基本的な動きを確認する_スリープ(){
+		final StringBuffer sb = new StringBuffer();
+		sb.append("start:");
+		Observable.just(10)
+		.map(
+				// map
+				t -> { 
+					try {
+						Thread.sleep(500);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					sb.append("map"); return t;})
+		.subscribe(
+				// onNext
+				t -> sb.append("onNext"), 
+				// onError
+				error -> sb.append("onError"), 
+				// onComplete
+				() -> {
+					sb.append("onComplete");
+				});
+		sb.append("end");
+		
+		assertThat(sb.toString(), is("start:maponNextonCompleteend"));
+	}
 
+
+	
 }
